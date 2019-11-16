3cd77e81f7065a0bdf9d108232f2ea41d11862ca
David A <david.amelekh@protonmail.com>
2019-11-16 13:07:17 +0100

    test button on login

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,42 @@
 <template>
-  <q-page class="flex ">
-    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
-    <p>
-      Blah    Blah a
-      </p>
+  <q-page>
+    <div v-if="account">
+      logged
+      <q-btn color="primary" @click="blah">
+        blah
+      </q-btn>
+    </div>
+    <div v-else>
+      not logged
+    </div>
   </q-page>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+import { aggregates } from 'aleph-js'
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  computed: {
+      ... mapState([
+        // map this.count to store.state.count
+        'account',
+        'network_id',
+        'api_server',
+        'channel'
+      ])
+  },
+  methods: {
+    async blah() {
+      console.log("blah")
+      await aggregates.submit(this.account.address, "test", {
+        'blah': 'bluh'
+      }, {
+        api_server: this.api_server,
+        account: this.account,
+        channel: this.channel
+      })
+    }
+  }
 }
 </script>
