78c8760cc8a41d7bf741c10867d610c14b916304
Moshe Malawach <moshe.malawach@protonmail.com>
2019-11-20 22:16:04 +0100

    notes list to separate component

---
+++ src/components/NotesList.vue
@@ -0,0 +1,33 @@
+<template>
+  <q-list padding>
+    <q-item-label header>Notes</q-item-label>
+    <template v-for="item in notes">
+      <q-item :to="{'name': 'edit-note', params:{'hash': item.hash}}" :key="item.hash+'it'" clickable>
+        <q-item-section>
+        <q-item-label>{{item.content.title}}</q-item-label>
+        <q-item-label overline>
+            {{item.time * 1000 | moment("from")}}
+            <q-tooltip>{{item.time * 1000 | moment("LLL")}}</q-tooltip>
+        </q-item-label>
+        <q-item-label caption lines="3">{{item.content.body}}</q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+        <q-icon v-if="item.content.private" name="lock" size="xs" color="positive" />
+        <q-icon v-else name="lock_open" size="xs" />
+        </q-item-section>
+      </q-item>
+
+      <q-separator spaced inset :key="item.hash+'sep'" />
+    </template>
+  </q-list>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import { aggregates, posts, encryption } from 'aleph-js'
+export default {
+  name: 'notes-list',
+  props: ['notes']
+}
+</script>
