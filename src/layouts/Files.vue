522f7b5ca3f381c61f8e58e9f9a439d84d6da1ad
Moshe Malawach <moshe.malawach@protonmail.com>
2019-12-02 17:57:54 +0100

    files layout

---
+++ src/layouts/Files.vue
@@ -0,0 +1,49 @@
+<template>
+  <q-page>
+    <div v-if="account">
+        <router-view />
+    </div>
+    <div v-else>
+      Please log in.
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import { aggregates, posts, encryption } from 'aleph-js'
+import NotesList from '../components/NotesList'
+export default {
+  name: 'FilesLayout',
+  computed: {
+    ... mapState([
+      // map this.count to store.state.count
+      'account',
+      'network_id',
+      'api_server',
+      'channel',
+      'files'
+    ]),
+    is_home() {
+      return (this.$route.name === 'files')
+    }
+  },
+  data() {
+    return {
+    }
+  },
+  components: {
+  },
+  methods: {
+  },
+  watch: {
+    async $route(to, from) {
+    },
+    async current_page() {
+    }
+  },
+  async created() {
+    // await this.refresh()
+  }
+}
+</script>
