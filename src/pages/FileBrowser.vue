5cda1dbe056f0045b6173de1b6104ec314339bae
Moshe Malawach <moshe.malawach@protonmail.com>
2019-12-04 17:38:32 +0100

    folder creation

--- src/pages/FileBrowser.vue
+++ src/pages/FileBrowser.vue
@@ -18,7 +18,8 @@
       To test the myaleph features, try the Notes section instead.
     </p>
     <div v-if="files.length">
-      <files-list :files="files" />
+      <files-list :files="files"
+      virtual-scroll flat />
     </div>
     <!-- <p v-else>
       No note here yet... Why not <router-link :to="{'name': 'new-note'}">write one</router-link>?
@@ -30,7 +31,7 @@
         direction="up"
         color="primary"
       >
-        <!-- <q-fab-action color="primary" icon="create_new_folder" /> -->
+        <q-fab-action @click="create_folder" color="primary" icon="create_new_folder" />
         <q-fab-action @click="upload" color="primary" icon="cloud_upload" />
       </q-fab>
     </q-page-sticky>
@@ -60,10 +61,14 @@ export default {
       'files'
     ])
   },
+  props: {
+    folder: String
+  },
   data() {
     return {
       upload_shown: false,
-      loading: false
+      loading: false,
+      current_folder: null
     }
   },
   components: {
@@ -88,6 +93,43 @@ export default {
       await sleep(10)
       this.$refs.uploader.pickFiles()
     },
+    async create_folder() {
+      this.$q.dialog({
+        // dark: true,
+        title: 'Create new folder',
+        message: 'Folder name:',
+        prompt: {
+          // model: '',
+          type: 'text' // optional
+        },
+        cancel: true,
+        persistent: true
+      }).onOk(async data => {
+        if (!data)
+          return
+        let post_content = {
+          filename: data,
+          ref: this.folder,
+          status: 'visible' // default status
+        }
+
+        let unencrypted_content = {}
+        Object.keys(post_content).forEach(function(key) {
+            unencrypted_content[ key ] = post_content[ key ]
+        })
+
+        encrypt_content(post_content, ['filename'], this.account['public_key'])
+
+        let msg = await posts.submit(
+          this.account.address, 'folder', post_content,
+          {channel: this.channel,
+           api_server: this.api_server,
+           account: this.account})
+        
+        msg.content = unencrypted_content
+        this.$store.commit('add_file', msg)
+      })
+    },
     async uploaded(info) {
       this.upload_shown = false
       for (let file of info) {
@@ -95,6 +137,7 @@ export default {
           filename: file.name,
           mimetype: file.type,
           private: file.private,
+          ref: this.folder,
           store_message: file.message_hash,
           hash: file.item_hash,
           engine: file.item_type,
