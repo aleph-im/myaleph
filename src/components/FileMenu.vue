b5f73428ea6337f299c2a6757870a4d69d8ed1b4
Moshe Malawach <moshe.malawach@protonmail.com>
2019-12-09 23:10:11 +0100

    move file menu to separate component

---
+++ src/components/FileMenu.vue
@@ -0,0 +1,92 @@
+<template>
+  <q-btn :size="size?size:'12px'" flat dense round icon="more_vert">
+      <q-menu>
+      <q-list style="min-width: 100px">
+          <q-item clickable v-close-popup @click="download(file)"  v-if="file.original_type === 'file'">
+          <q-item-section avatar>
+              <q-icon color="primary" name="cloud_download" />
+          </q-item-section>
+          <q-item-section>Download</q-item-section>
+          </q-item>
+          <q-item clickable v-close-popup @click.prevent="archive(file)" v-if="file.content.status === 'visible'">
+          <q-item-section avatar>
+              <q-icon color="primary" name="archive" />
+          </q-item-section>
+          <q-item-section>Archive</q-item-section>
+          </q-item>
+          <q-item clickable v-close-popup @click.prevent="unarchive(file)" v-else>
+          <q-item-section avatar>
+              <q-icon color="primary" name="unarchive" />
+          </q-item-section>
+          <q-item-section>Un-Archive</q-item-section>
+          </q-item>
+      </q-list>
+      </q-menu>
+  </q-btn>
+</template>
+
+
+<script>
+import { mapState } from 'vuex'
+import { aggregates, posts, encryption, store } from 'aleph-js'
+import { encrypt_content, decrypt_content } from '../services/encryption.js'
+import moment from 'moment'
+import {download_file} from '../services/files'
+import { format } from 'quasar'
+const { humanStorageSize } = format
+export default {
+  name: 'files-menu',
+  props: {
+    'file': Object,
+    'size': String
+  },
+  computed: {
+    ... mapState([
+      // map this.count to store.state.count
+      'account',
+      'network_id',
+      'api_server',
+      'channel'
+    ])
+  },
+  methods: {
+    async download(filepost) {
+      this.$q.loadingBar.start()
+      await download_file(filepost, this.account, this.api_server)
+      this.$q.loadingBar.stop()
+    },
+    async archive(filepost) {
+      await this.change_status(filepost, 'archived')
+    },
+    async unarchive(filepost) {
+      await this.change_status(filepost, 'visible')
+    },
+    async change_status(filepost, new_status) {
+      let post_content = {};
+      Object.keys(filepost.content).forEach(function(key) {
+          post_content[key] = filepost.content[key]
+      })
+      post_content['status'] = new_status
+      let unencrypted_content = {};
+      Object.keys(filepost.content).forEach(function(key) {
+          unencrypted_content[key] = post_content[key]
+      })
+
+      if (post_content.private)
+        encrypt_content(post_content, ['filename', 'mimetype', 'thumbnail_url'], this.account['public_key'])
+
+      let msg = await posts.submit(
+        this.account.address, 'amend', post_content,
+        {channel: this.channel,
+          api_server: this.api_server,
+          account: this.account,
+          ref:filepost.hash})
+      msg.hash = filepost.hash
+      msg.original_type = filepost.original_type
+      msg.content = unencrypted_content
+      msg.original_ref = filepost.original_ref
+      this.$store.commit('update_file', msg)
+    }
+  }
+}
+</script>
--- src/components/FilesList.vue
+++ src/components/FilesList.vue
@@ -3,7 +3,6 @@
     <q-item-label header v-if="title">{{title}}</q-item-label>
     <template v-for="(file, index) of files">
       <q-item :key="file.item_hash"
-              :to="file.original_type === 'folder' ? {'name': 'folder', params: {'folder': file.hash}} : null"
               clickable :class="file.content.status === 'archived' ? 'text-grey-8 text-strike' : ''">
         <q-item-section avatar top @click="$emit('item-clicked', file)">
             <q-avatar rounded size="48px" v-if="file.content.thumbnail_url">
@@ -21,43 +20,7 @@
         </q-item-section>
         <q-item-section top side>
           <div class="text-grey-8 q-gutter-xs">
-            <q-btn flat dense round class="gt-xs"
-                  size="12px" icon="cloud_download" @click="download(file)"
-                  v-if="file.original_type === 'file'">
-              <q-tooltip>Download file</q-tooltip>
-            </q-btn>
-            <q-btn class="gt-xs" size="12px" flat dense round icon="archive"
-                  @click.prevent="archive(file)" v-if="file.content.status === 'visible'">
-              <q-tooltip>Archive file</q-tooltip>
-            </q-btn>
-            <q-btn class="gt-xs" size="12px" flat dense round icon="unarchive"
-                  @click.prevent="unarchive(file)" v-else>
-              <q-tooltip>Un-Archive file</q-tooltip>
-            </q-btn>
-            <q-btn size="12px" class="lt-sm" flat dense round icon="more_vert">
-              <q-menu>
-                <q-list style="min-width: 100px">
-                  <q-item clickable v-close-popup @click="download(file)"  v-if="file.original_type === 'file'">
-                    <q-item-section avatar>
-                      <q-icon color="primary" name="cloud_download" />
-                    </q-item-section>
-                    <q-item-section>Download</q-item-section>
-                  </q-item>
-                  <q-item clickable v-close-popup @click.prevent="archive(file)" v-if="file.content.status === 'visible'">
-                    <q-item-section avatar>
-                      <q-icon color="primary" name="archive" />
-                    </q-item-section>
-                    <q-item-section>Archive</q-item-section>
-                  </q-item>
-                  <q-item clickable v-close-popup @click.prevent="unarchive(file)" v-else>
-                    <q-item-section avatar>
-                      <q-icon color="primary" name="unarchive" />
-                    </q-item-section>
-                    <q-item-section>Un-Archive</q-item-section>
-                  </q-item>
-                </q-list>
-              </q-menu>
-            </q-btn>
+            <file-menu :file="file" size="12px" />
           </div>
         </q-item-section>
       </q-item>
@@ -70,6 +33,7 @@
 import { mapState } from 'vuex'
 import { aggregates, posts, encryption, store } from 'aleph-js'
 import { encrypt_content, decrypt_content } from '../services/encryption.js'
+import FileMenu from './FileMenu'
 import moment from 'moment'
 import {download_file} from '../services/files'
 import { format } from 'quasar'
@@ -92,44 +56,8 @@ export default {
       'channel'
     ])
   },
+  components: {FileMenu},
   methods: {
-    async download(filepost) {
-      this.$q.loadingBar.start()
-      await download_file(filepost, this.account, this.api_server)
-      this.$q.loadingBar.stop()
-    },
-    async archive(filepost) {
-      await this.change_status(filepost, 'archived')
-    },
-    async unarchive(filepost) {
-      await this.change_status(filepost, 'visible')
-    },
-    async change_status(filepost, new_status) {
-      let post_content = {};
-      Object.keys(filepost.content).forEach(function(key) {
-          post_content[key] = filepost.content[key]
-      })
-      post_content['status'] = new_status
-      let unencrypted_content = {};
-      Object.keys(filepost.content).forEach(function(key) {
-          unencrypted_content[key] = post_content[key]
-      })
-
-      if (post_content.private)
-        encrypt_content(post_content, ['filename', 'mimetype', 'thumbnail_url'], this.account['public_key'])
-
-      let msg = await posts.submit(
-        this.account.address, 'amend', post_content,
-        {channel: this.channel,
-          api_server: this.api_server,
-          account: this.account,
-          ref:filepost.hash})
-      msg.hash = filepost.hash
-      msg.original_type = filepost.original_type
-      msg.content = unencrypted_content
-      msg.original_ref = filepost.original_ref
-      this.$store.commit('update_file', msg)
-    }
   },
   data() {
     return {
