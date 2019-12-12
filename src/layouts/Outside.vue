8d70a68fd12c6c5d8a0bce23b7a900166fa35edc
Moshe Malawach <moshe.malawach@protonmail.com>
2019-12-12 17:00:24 +0100

    adding outside layout placeholder

---
+++ src/layouts/Outside.vue
@@ -0,0 +1,20 @@
+<template>
+  <router-view />
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  name: 'OutsideLayout',
+  computed: {
+    ... mapState([
+      // map this.count to store.state.count
+      'account',
+      'network_id',
+      'api_server',
+      'channel',
+      'files'
+    ])
+  }
+}
+</script>
