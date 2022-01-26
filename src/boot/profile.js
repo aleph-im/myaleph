3384cdbea5ea80175b8f1e4587fd52abecd53e7f
Moshe Malawach <moshe.malawach@protonmail.com>
2022-01-26 15:12:25 +0100

    fix profile fetch

--- src/boot/profile.js
+++ src/boot/profile.js
@@ -3,7 +3,12 @@ import Vue from 'vue'
 import {aggregates} from 'aleph-js'
 
 Vue.prototype.$fetch_profile = async function(address) {
-    let profile = await aggregates.fetch_profile(address, {api_server: this.$store.state.api_server});
+    let profile = {}
+    try {
+        profile = await aggregates.fetch_profile(address, {api_server: this.$store.state.api_server});
+    } catch {
+        
+    }
     this.$store.commit('store_profile', {
         address: address,
         profile: profile
